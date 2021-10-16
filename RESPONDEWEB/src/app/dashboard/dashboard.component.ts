2fd2548f051b913636077c7310b7102d9246529e
Levi Sazon <sazon.levi19@gmail.com>
2021-10-17 06:33:42 +0800

    incoming report table to dashboard

--- RESPONDEWEB/src/app/dashboard/dashboard.component.ts
+++ RESPONDEWEB/src/app/dashboard/dashboard.component.ts
@@ -1,21 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DataSource } from '@angular/cdk/table';
 import { AngularFireDatabase, AngularFireList, AngularFireObject} from '@angular/fire/database';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { Routes, RouterModule, Router} from '@angular/router';
+import { FullIncomingReportComponent } from '../full-incoming-report/full-incoming-report.component';
+import { AuthService } from '../auth.service'
+
+
+export interface IncomingReport {
+  key?: string;
+  home?: string;
+  contact?: string;
+  date?: string;
+  lat?: string;
+  lng?: string;
+  name?: string;
+  option?: string;
+  email?: string;
+  status?: string;
+}; 
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements AfterViewInit {
   dataSource : any;
   incomingReportLength: any;
   reportHistoryLength: any;
-  constructor(private authService: AuthService, private db: AngularFireDatabase) {
+
+  displayedColumns: string[] = ['key','date','name','home','contact','actions'];
+  IncomingReport?:  IncomingReport[];
+
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort?: MatSort;
+
+  constructor(private authService: AuthService, private db: AngularFireDatabase, private router: Router) {
 
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.db.list('/IncomingReport/', ref => ref.orderByChild("status").equalTo('')).snapshotChanges().subscribe(data => {
       this.incomingReportLength=(data.length); 
       console.log(data); 
@@ -25,6 +55,42 @@ export class DashboardComponent implements OnInit {
       this.reportHistoryLength=(data.length);
       console.log(this.reportHistoryLength);
     });
+
+    this.dataSource = new MatTableDataSource<IncomingReport>([]);
+
+    this.db.list('IncomingReport/', ref => ref.orderByChild('status').equalTo(''))
+    .snapshotChanges().subscribe(data => {
+      this.dataSource.data = data
+
+      this.IncomingReport = [];
+      console.log(data);
+      data.forEach(item => {
+        let a = item.payload.key;
+        let val:any = item.payload;
+        this.IncomingReport!.push(val as IncomingReport);
+        
+      })
+    })
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  logData(row: any){
+    console.log(row);
+  }
+  
+  // table filtering
+  applyFilter(event: Event): string {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    return (event.target as HTMLInputElement).value;
+  }
+
+  /* view route */
+  onSelect(report){
+    this.router.navigate(['incoming-report', report.key]);
+  }
 }
