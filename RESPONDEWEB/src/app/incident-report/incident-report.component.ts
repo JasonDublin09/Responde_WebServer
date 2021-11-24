d8f1fb9352720e604af1a5c9013d6b814bdcb124
Levi Sazon <sazon.levi19@gmail.com>
2021-11-24 19:54:34 +0800

    incident report table fixes

--- RESPONDEWEB/src/app/incident-report/incident-report.component.ts
+++ RESPONDEWEB/src/app/incident-report/incident-report.component.ts
@@ -11,12 +11,36 @@ import { AuthService } from '../auth.service';
 import { AliasStrategy } from '@angular/compiler-cli/src/ngtsc/imports';
 /* form */
 
+
+
+export interface IncidentReport {
+  key?: string;
+  address?: string;
+  casualtyCivilian?: string;
+  casualtyFirefighter?: string;
+  date_incident?: string;
+  date_prepared?: string;
+  house?: string;
+  injuredCivilian?: string;
+  injuredFirefighter?: string;
+  name?: string;
+  other?: string;
+  time_extinguised?: string;
+  time_start?: string;
+};
 @Component({
   selector: 'app-incident-report',
   templateUrl: './incident-report.component.html',
   styleUrls: ['./incident-report.component.css']
 })
-export class IncidentReportComponent implements OnInit {
+
+export class IncidentReportComponent implements AfterViewInit {
+
+  displayedColumns: string[] = ['key','date_prepared','name','address','date_incident','house','injuredCivilian','casualtyCivilian',
+  'injuredFirefighter','casualtyFirefighter','time_start','time_extinguished','other'];
+
+  IncidentReport?:  IncidentReport[];
+  dataSource : any;
 
   public date_prepared: any;
   public name: any;
@@ -28,16 +52,52 @@ export class IncidentReportComponent implements OnInit {
   public injuredFirefighter: any;
   public casualtyFirefighter: any;
   public time_start: any;
-  public time_extinguised: any;
+  public time_extinguished: any;
   public other: any;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild('myModalClose') modalClose;
 
   constructor(private db: AngularFireDatabase, private authService: AuthService, private router:Router) { }
   
-  @ViewChild('myModalClose') modalClose;
-
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<IncidentReport>([]);
+    console.log(this.dataSource);
+    console.log(this.dataSource.filteredData);
+
+    this.db.list('IncidentReport/')
+    .snapshotChanges().subscribe(data => {
+      this.dataSource.data = data;
+      
+      this.IncidentReport = [];
+      data.forEach(item => {
+        let a = item.payload.key;
+        let val:any = item.payload;
+        
+        this.IncidentReport!.push(val as IncidentReport);
+        
+      })
+    })
+
+    
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  logData(row: any){
+    console.log(row);
+  }
+
+  // table filtering
+  applyFilter(event: Event): string {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    return (event.target as HTMLInputElement).value;
+  }
   onSubmit(){
     let date_prepared = (<HTMLInputElement>document.getElementById("dateprepared")).value;
     let name = (<HTMLInputElement>document.getElementById("name")).value;
@@ -49,7 +109,7 @@ export class IncidentReportComponent implements OnInit {
     let injuredFirefighter = (<HTMLInputElement>document.getElementById("injuredresponder")).value;
     let casualtyFirefighter = (<HTMLInputElement>document.getElementById("casualtyresponder")).value;
     let time_start = (<HTMLInputElement>document.getElementById("start")).value;
-    let time_extinguised = (<HTMLInputElement>document.getElementById("ext")).value;
+    let time_extinguished = (<HTMLInputElement>document.getElementById("ext")).value;
     let other = (<HTMLInputElement>document.getElementById("other")).value;
 
     this.date_prepared = date_prepared
@@ -62,17 +122,17 @@ export class IncidentReportComponent implements OnInit {
     this.injuredFirefighter = injuredFirefighter
     this.casualtyFirefighter = casualtyFirefighter
     this.time_start = time_start
-    this.time_extinguised = time_extinguised
+    this.time_extinguished = time_extinguished
     this.other = other;
 
     if (date_prepared != "" && name != "" && address != "" && date_incident != "" && house != "" && injuredCivilian != "" &&
-        casualtyCivilian != "" && injuredFirefighter != "" && casualtyFirefighter != "" && time_start != "" && time_extinguised != "") {
+        casualtyCivilian != "" && injuredFirefighter != "" && casualtyFirefighter != "" && time_start != "" && time_extinguished != "") {
           // success
 
           this.authService.getIncidentReportList()
             .push({date_prepared:this.date_prepared, name:this.name, address:this.address, date_incident:this.date_incident,
             house:this.house, injuredCivilian:this.injuredCivilian, casualtyCivilian:this.casualtyCivilian, injuredFirefighter:this.injuredFirefighter,
-            casualtyFirefighter:this.casualtyFirefighter, time_start:this.time_start, time_extinguised:this.time_extinguised, other:this.other});
+            casualtyFirefighter:this.casualtyFirefighter, time_start:this.time_start, time_extinguished:this.time_extinguished, other:this.other});
 
           alert('Incident Report created successfully!');
           this.modalClose.nativeElement.click();
@@ -81,5 +141,7 @@ export class IncidentReportComponent implements OnInit {
     }
     
   }
+
+  
 
 }
